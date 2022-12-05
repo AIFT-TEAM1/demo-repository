0a8d928052bde3eac5855b192031d0d97fb5ede0
jooyyoo <aliceyun9@naver.com>
2022-12-05 10:26:16 +0900

    Create ui.py

---
+++ ui/ui.py
@@ -0,0 +1,13 @@
+import sys
+from kiwoom.kiwoom import *
+from PyQt5.QtWidgets import *
+
+class ui_class():
+    def __init__(self):
+        print("ui 실행")
+
+        self.app = QApplication(sys.argv)
+
+        self.kiwoom = Kiwoom()
+
+        self.app.exec_() #이벤트 루프 / 종료를 막아줌
